1fabbb02f387da0cc77188d2222e76cd3e61c7c4
yo76317 <yo76317@gmail.com>
2021-12-10 15:04:29 +0800

    物件導向拿到送出大型專案可省時間

--- oop.php
+++ oop.php
@@ -1,20 +1,44 @@
 <?php
-
+// 條件式
 class Animal{
     public $name='';
     protected $age=0;
-    private $heart_beat;
+    private $heartbeat;
 
     // 建構式
-    public function __construct(){
-        $this->age=rand(10,20);
-        $this->name='yo';
-        $this->heartbeat=rand(20,60);
+    public function getName(){
+       return $this->name;
     }
+    public function getheartbeat(){
+       return $this->heartbeat;
+    }
+    public function setName($name){
+       $this->name=$name;
+    }
+
 }
 
 $animal=new Animal;
+$dog=new Animal;
+// $this->age=rand(10,20);
+// $this->name='yo';
+// $this->heartbeat=rand(20,60);
+// $animal=new Animal;
 
-echo $animal->name;
+// echo $animal->name;
 
+// 受保護内部使用
+// echo $animal->age;
+
+// 改變值，不好因為難找在那改變的
+// $animal->name="qing"
+// echo $animal->name;
+
+echo $animal->getName();
+$animal->setName('yo');
+echo $animal->getName();
+echo $animal->getHeartbeat();
+echo "<hr>";
+$dog->setName('qing');
+echo $dog->getName();
 ?>

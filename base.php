7fce53591e5be50171aeaafe3a8d9f562e9ccbad
yo76317 <yo76317@gmail.com>
2021-12-10 16:09:35 +0800

    可叫出資料表陣列

--- base.php
+++ base.php
@@ -1,10 +1,12 @@
 <?php
 
+//條件
 class DB{
     protected $table='';
-    protected $dsn="mysql:host=localhost;chartset=utf8;dbname=member";
+    protected $dsn="mysql:host=localhost;charset=utf8;dbname=member";
     protected $pdo;
 
+    //構成
     // $db=new DB ('users');
     public function __construct($table){
         // 把pdo帶進來
@@ -12,11 +14,15 @@ class DB{
         $this->table=$table;
     }
 
-    public function getTable(){
-        return $this->table;
+    public function all(){
+        $rows=$this->pdo->query("select * from $this->table")->fetchAll(PDO::FETCH_ASSOC);
+
+        return $rows;
     }
 }
 
 $db=new DB ('member');
-echo $db->getTable();
+echo "<pre>";
+print_r($db->all());
+echo "</pre>";
 ?>
